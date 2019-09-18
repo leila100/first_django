0c3738ac40ddc0f9a44697f73bb7078d9d8cc099
Leila <nb.leila10@gmail.com>
2019-09-18 17:31:59 -0500

    Add jobs route and filter Results by User

--- jobs/api.py
+++ jobs/api.py
@@ -6,10 +6,22 @@ from .models import PersonalJob
 class PersonalJobSerializer(serializers.HyperlinkedModelSerializer):
     # Inner class nested inside PersonalNoteSerializer
     class Meta:
-        model = PersonalNote
+        model = PersonalJob
         fields = ('title', 'company', 'description')
 
 
 class PersonalJobViewSet(viewsets.ModelViewSet):
     serializer_class = PersonalJobSerializer
-    queryset = PersonalNote.objects.all()
+    queryset = PersonalJob.objects.none()
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        job = PersonalJob.objects.create(user=user, **validated_data)
+        return job
+
+    def get_queryset(self):
+        user = self.request.user
+        print(user)
+        if user.is_anonymous:
+            return PersonalJob.objects.none()
+        return PersonalJob.objects.filter(user=user)
